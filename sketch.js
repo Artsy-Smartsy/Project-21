11c232329485ffb69c8fe33f29035b14da0e8b3e
Artsy! <84583236+Artsy-Smartsy@users.noreply.github.com>
2021-06-16 04:16:07 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,64 @@
+var grid = 50;
+var width = 1366;
+var carGroup1,logGroup1;
+var grassHeight = 100;
+var gameState = "play";
+var carAnimation, logAnimation, playerAnimation;
+var school;
+function preload()
+{
+ 
+}
+
+function setup() {
+  createCanvas(1366,700);
+  carGroup1 = new Group();
+  logGroup1 = new Group();
+  
+  for(var i=0;i<6;i++){
+    var bottomGrass1 = createSprite(683,height-50-(i*400),width,grassHeight);
+    if(i%2===0)//adding road
+    {
+     var road= createSprite(683,height-150-(i*400)-grassHeight,width,300,)
+      road.shapeColor="black";
+    }
+    bottomGrass1.shapeColor = "green";
+  }
+  //To create rows of car
+   for(var i = 0; i < 40; i++){
+     cars = new Car(4);
+     carGroup1.add(cars.spt);
+   }
+  //To create rows of Logs
+    for(var i = 0; i < 40; i++){
+      log = new Log(-4);
+      logGroup1.add(log.spt);
+    }
+ }
+
+function draw() {
+  background("skyblue");
+
+
+  //Making the cars re-apper
+  for(i=10;i<carGroup1.length;i++) {
+    if(carGroup1[i].x>width)
+    {
+     carGroup1[i].x=0;
+    }
+    if(carGroup1[i].x<0)
+    {
+      carGroup1[i].x=width;
+    }
+  }
+
+  //making the logs re-apper
+  for(i=random([1,3,4]);i<logGroup1.length;i++){
+    if(logGroup1[i].x<0)
+    {
+    logGroup1[i].x=width;
+    }
+  }
+ 
+  drawSprites();
+}
